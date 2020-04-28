8cc68183ff75769c31e83a4c3126fdfb706dabb3
James Robertson <jamesurobertson@gmail.com>
2020-04-28 00:04:46 -0500

    learning about sql / postgreSQL

--- 01-create-users.sql
+++ 01-create-users.sql
@@ -7,9 +7,13 @@
 -- | project_management_app | Q3HphPJ8 |
 
 
+create user aa_times with password '6g73wE2V';
+create user project_management_app with password 'Q3HphPJ8';
 
 
 -- Create the following superuser with the indicated name and password
 -- | name       | password |
 -- |------------|----------|
 -- | data_admin | ik2fiDri |
+
+create user data_admin with superuser password 'ik2fiDri';
--- 03-create-aa-times-tables.sql
+++ 03-create-aa-times-tables.sql
@@ -12,7 +12,13 @@ SET ROLE aa_times;
 -- | last_name   | VARCHAR(50)  | NOT NULL    |
 -- | email       | VARCHAR(100) | NOT NULL    |
 
-
+create table people (
+  id serial,
+  first_name varchar(50) not null,
+  last_name varchar(50) not null,
+  email varchar(100) not null,
+  primary key(id)
+);
 
 -- The "sections" table
 -- | Column name | Type         | Constraints |
@@ -20,6 +26,11 @@ SET ROLE aa_times;
 -- | id          | SERIAL       | PRIMARY KEY |
 -- | name        | VARCHAR(150) | NOT NULL    |
 
+create table sections (
+  id serial,
+  name varchar(150) not null,
+  primary key(id)
+);
 
 
 -- The "stories" table
@@ -29,3 +40,13 @@ SET ROLE aa_times;
 -- | author_id   | INTEGER | FOREIGN KEY to people table, NOT NULL   |
 -- | content     | TEXT    | NOT NULL                                |
 -- | section_id  | INTEGER | FOREIGN KEY to sections table, NOT NULL |
+
+create table stories (
+  id serial,
+  author_id integer not null,
+  content text not null,
+  section_id integer not null,
+  primary key(id),
+  foreign key(author_id) references people(id),
+  foreign key(section_id) references sections(id)
+);
